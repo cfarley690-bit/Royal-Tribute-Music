4582db7099f69e4509f0ffcbb50407dc481842e6
cfarley690-bit <cfarley690@gmail.com>
2025-10-02 14:31:36 -0400

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://shiva:shiva@musicbotyt.ou1jywv.mongodb.net/?retryWrites=trues&w=majority",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
